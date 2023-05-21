4f322f87cc825bae171492383a4a9d2b817a3879
hh <hf.zheng@scrypt.io>
2023-05-22 00:18:00 +0800

    add DotwalletSigner example

---
+++ src/app/callback/route.ts
@@ -0,0 +1,35 @@
+import { redirect, notFound } from 'next/navigation';
+
+import { writeFileSync } from "fs"
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get('code');
+
+  if (code) {
+    const res = await fetch(`https://api.ddpurse.com/v1/oauth2/get_access_token`, {
+      body: JSON.stringify({
+        code,
+        redirect_uri: process.env.REDIRECT_URI,
+        grant_type: "authorization_code",
+        client_secret: process.env.CLIENT_SECRET,
+        client_id: process.env.CLIENT_ID,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST'
+    });
+    const { code: apiCode, data, msg } = await res.json();
+
+    if (apiCode === 0) {
+      const { access_token } = data;
+      writeFileSync('access_token.txt', access_token);
+
+      redirect('/');
+    }
+
+  }
+
+  notFound();
+}
